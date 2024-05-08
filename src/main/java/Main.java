b03f5ee80cd40019c809bf894d2dcb4dd9dcc86a
xshigenx <161622150+xshigenx@users.noreply.github.com>
2024-05-08 15:04:54 +0000

    Wpisywanie na listę

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,20 +8,35 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner = new Scanner(System.in);
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      System.out.println("Wybierz opcję:");
+      System.out.println("1. Dodaj studenta");
+      int option = scanner.nextInt();
+      scanner.nextLine(); 
+
+      switch(option) {
+        case 1:
+          System.out.println("Podaj imię studenta:");
+          String name = scanner.nextLine();
+          System.out.println("Podaj wiek studenta:");
+          int age = scanner.nextInt();
+          scanner.nextLine(); 
+          s.addStudent(new Student(name, age));
+          System.out.println("Student dodany.");
+          break;
+          
+        default:
+          System.out.println("Niepoprawna opcja.");
       }
     } catch (IOException e) {
-
+      e.printStackTrace();
     }
   }
 }
